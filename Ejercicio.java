c3354d9711933eb2ac29dc0d14b7e0183c2e9b38
Flix-23 <123584809+Flix-23@users.noreply.github.com>
2024-02-15 09:31:04 -0400

    Actualizar Ejercicio.java

--- Ejercicio.java
+++ Ejercicio.java
@@ -2,8 +2,8 @@ public class Ejercicio {
     public static void main(String[] args){
         int[] numeros= new int[100];
         
-        for(int indice = 0; indice < 100; indice++){
-        System.out.println("Los numeros son: " + (indice + 1) );
+        for(int indice = 0; indice < numeros; indice++){
+                System.out.println("Los numeros son: " + indice);
     }
     
 }
